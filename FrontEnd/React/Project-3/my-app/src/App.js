4473a1bd7bfbb8868f9bb0e407362ca335f75363
anurag-int <ag6526803@gmail.com>
2023-07-22 21:06:30 +0530

    Project with the use of useState, useEffect

---
+++ FrontEnd/React/Project-3/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [text, setText] = useState('');
+
+
+  useEffect( () => {
+      console.log("UI Rendering Done");   
+  });
+
+  function changeHandler(event) {
+    console.log(text);
+    setText(event.target.value);
+  }
+  return (
+    <div className="App">
+      <input type="text" onChange={changeHandler}></input>
+    </div>
+  );
+}
+
+export default App;
